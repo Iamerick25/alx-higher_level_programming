d24d9bcdcc6090663b8e27e1f3bf548f00bd320a
Iamerick25 <erickchinyem@gmail.com>
2023-11-21 07:36:10 -0800

    2-square

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,6 +1,12 @@
 #!/usr/bin/python3
+"""The square module continue"""
+
 class Square:
+"""The class to create square."""
+
     def __init__(self, size=0):
+        """set size to private instance variable"""
+
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         elif size < 0:
