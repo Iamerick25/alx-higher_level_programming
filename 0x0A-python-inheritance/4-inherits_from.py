eb22230e108960a253bb352818b18c874737c10e
Iamerick25 <erickchinyem@gmail.com>
2023-12-04 13:28:14 -0800

    Correction on task 4

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -2,17 +2,5 @@
 """Module implementing function to determine subclass membership"""
 
 def inherits_from(obj, a_class):
-    """
-    Returns True if the object is an instance of a class that inherited
-    (directly or indirectly) from the specified class;
-    otherwise, False.
-
-    Parameters:
-    - obj: The object to check.
-    - a_class: The class to compare against.
-
-    Returns:
-    - True if obj is an instance of a class that inherited from a_class;
-    otherwise, False.
-    """
+    """check if it inherited instance of that class"""
     return (issubclass(type(obj), a_class) and type(obj) != a_class)
