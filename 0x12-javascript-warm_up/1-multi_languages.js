838626ad2ee1002c0d00c41d56483a2de5738962
root <erickchinyem@gmail.com>
2024-02-13 02:22:13 -0800

    Correction on Task1

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-// script that prints 3 lines
+// a script that prints 3 lines
 console.log('C is fun');
 console.log('Python is cool');
-Python is cool('JavaScript is amazing');
+console.log('JavaScript is amazing');
